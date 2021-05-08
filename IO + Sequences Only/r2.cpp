510737a52660ff54d50575baf0433425a1f07f76
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:17:50 +0600

    Create r2.cpp

---
+++ IO + Sequences Only/r2.cpp
@@ -0,0 +1,18 @@
+//https://open.kattis.com/problems/r2
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<(2*b-a)<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Main();
+    return 0;
+}
