829e334a930a18dd8fb3f188f5bcef80e0917b01
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:45:16 +0600

    Create different.cpp

---
+++ Chapter 1/Repetition Only/different.cpp
@@ -0,0 +1,20 @@
+//https://open.kattis.com/problems/different
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void Main(){
+    ll a,b;
+    while(cin>>a>>b){
+        cout<<abs(a-b)<<"\n";
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Main();
+    return 0;
+}
