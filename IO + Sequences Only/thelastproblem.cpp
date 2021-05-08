91928794d2b8ee0c9cea7617a5d86b65174ddf5f
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:14:28 +0600

    Create thelastproblem.cpp

---
+++ IO + Sequences Only/thelastproblem.cpp
@@ -0,0 +1,18 @@
+//https://open.kattis.com/problems/thelastproblem
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+	string s;
+	getline(cin,s);
+	cout<<"Thank you, "<<s<<", and farewell!\n";	
+}
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	Main();
+	return 0;
+}
