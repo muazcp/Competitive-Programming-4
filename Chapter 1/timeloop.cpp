4d8040c944e82625268ac11509eb0ab78b22b411
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:40:17 +0600

    Create timeloop.cpp

---
+++ Chapter 1/timeloop.cpp
@@ -0,0 +1,20 @@
+//https://open.kattis.com/problems/timeloop
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<i<<" Abracadabra\n";
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Main();
+    return 0;
+}
