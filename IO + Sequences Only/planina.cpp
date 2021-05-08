ff0d01a60782b48915a310c3a35207d57af187ea
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:20:45 +0600

    Create planina.cpp

---
+++ IO + Sequences Only/planina.cpp
@@ -0,0 +1,18 @@
+//https://open.kattis.com/problems/planina
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+    int n;
+    cin>>n;
+    cout<<((1<<n)+1)*((1<<n)+1)<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Main();
+    return 0;
+}
