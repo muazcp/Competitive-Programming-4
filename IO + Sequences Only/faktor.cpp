3e6e86b0baa3e661c186bbd71ec35933a3b30080
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:16:07 +0600

    Create faktor.cpp

---
+++ IO + Sequences Only/faktor.cpp
@@ -0,0 +1,18 @@
+//https://open.kattis.com/problems/faktor
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+    int a,i;
+    cin>>a>>i;
+    cout<<a*(i-1)+1<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Main();
+    return 0;
+}
