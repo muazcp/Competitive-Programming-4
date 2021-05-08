4e0b0ed0d8e369b3cdf41119d59dd7863c2ecb88
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:05:54 +0600

    Create carrots.cpp

---
+++ IO + Sequences Only/carrots.cpp
@@ -0,0 +1,18 @@
+//https://open.kattis.com/problems/carrots
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<b<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Main();
+    return 0;
+}
