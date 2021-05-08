a6e0ba1cbbd96f77bd9f0d9bf156a5370c317665
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-08 12:03:47 +0600

    Create hello.cpp

---
+++ IO + Sequences Only/hello.cpp
@@ -0,0 +1,16 @@
+//https://open.kattis.com/problems/hello
+//Date: 08/05/2021
+//Author: Muaz
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+    cout<<"Hello World!\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    Main();
+    return 0;
+}
