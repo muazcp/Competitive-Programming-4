1b3b8703dfbbecacd5b36d9c518154de7537e904
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-05-13 08:59:09 +0600

    Create uva11614.cpp

---
+++ Chapter 1/IO + Sequences Only/uva11614.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+	long long n,t;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		cout<<(int)(-1+sqrt(1+8*n))/2<<"\n";
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	Main();
+	return 0;
+}
