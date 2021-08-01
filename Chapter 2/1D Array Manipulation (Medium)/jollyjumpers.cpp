61c35644785d74ca7551ec10b6fec2b9143adf2d
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-08-01 16:32:41 +0600

    Update jollyjumpers.cpp

--- Chapter 2/1D Array Manipulation (Medium)/jollyjumpers.cpp
+++ Chapter 2/1D Array Manipulation (Medium)/jollyjumpers.cpp
@@ -1,4 +1,36 @@
-//link:
-//Date:
-//Author:
+//link: https://open.kattis.com/problems/jollyjumpers
+//Date: 01/08/2021
+//Author: Muaz
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+	int n;
+	while(cin>>n){
+		vector<int> ar(n);
+		for(int& i:ar){
+			cin>>i;
+		}
+		vector<bool> mark(n,0);
+		for(int i=1;i<n;i++){
+			mark[abs(ar[i]-ar[i-1])]=1;
+		}
+		bool res=1;
+		for(int i=1;i<n;i++){
+			res&=mark[i];
+		}
+		if(res){
+			cout<<"Jolly\n";
+		}else{
+			cout<<"Not Jolly\n";
+		}
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	Main();
+	return 0;
+}
