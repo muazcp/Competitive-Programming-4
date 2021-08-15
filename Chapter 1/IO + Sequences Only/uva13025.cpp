5fc6de082972bd56e0f3b1cfc825cf18f2b0de10
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2021-08-15 19:58:45 +0600

    Create uva13025.cpp

---
+++ Chapter 1/IO + Sequences Only/uva13025.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Main(){
+	cout<<"May 29, 2013 Wednesday\n";	
+}
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	Main();
+	return 0;
+}
