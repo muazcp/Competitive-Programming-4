639825d7260d392e0e23026b991a86c9b6a9e83b
Muaz Hussain <82491592+muazcp@users.noreply.github.com>
2022-11-04 22:59:13 +0600

    Add files via upload

---
+++ Chapter 2/1D Array Manipulation (Harder)/UVA-12662.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<string> name(n);
+    // Some code goes here
+    int query;
+    cin >> query;
+    while(query--) {
+        int p;
+        cin >> p;
+        // Some code goes here
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    solve();
+    return 0;
+}
